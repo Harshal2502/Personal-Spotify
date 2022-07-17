e4d00bf50b1fb70552d03caee83e648ed0f50f31
HarshalDevmurari <92778686+HarshalDevmurari@users.noreply.github.com>
2022-07-17 15:50:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let songs = [
+    {songName: "barfi", FilePath: "songs/1.mp3", coverPath: "images/1.jpg"},
+    {songName: "barfi", FilePath: "songs/1.mp3", coverPath: "images/1.jpg"},
+    {songName: "barfi", FilePath: "songs/1.mp3", coverPath: "images/1.jpg"},
+    {songName: "barfi", FilePath: "songs/1.mp3", coverPath: "images/1.jpg"},
+    {songName: "barfi", FilePath: "songs/1.mp3", coverPath: "images/1.jpg"},
+    {songName: "barfi", FilePath: "songs/1.mp3", coverPath: "images/1.jpg"},
+    {songName: "barfi", FilePath: "songs/1.mp3", coverPath: "images/1.jpg"},
+    {songName: "barfi", FilePath: "songs/1.mp3", coverPath: "images/1.jpg"},
+]
+let audioELement = new Audio('1.mp3');
